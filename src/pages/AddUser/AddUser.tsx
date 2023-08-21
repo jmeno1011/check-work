fcb85008f3acf2a16d6a54f5a9ae99353cd5439e
jmeno1011 <jwhltn8282@gmail.com>
2023-08-21 18:20:51 +0900

    add input form

--- src/pages/AddUser/AddUser.tsx
+++ src/pages/AddUser/AddUser.tsx
@@ -1,10 +1,25 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, TextInput, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import Title from '../../components/Title/Title';
 
 export default function AddUser() {
+  const [name, setName] = useState<string>('');
+  const onChangeName = (newName: string) => {
+    setName(newName);
+  };
   return (
     <View>
-      <Text>AddUser</Text>
+      <Title>유저 추가</Title>
+      <View style={{padding: 10, flexDirection: 'row', alignItems: 'center'}}>
+        <Text style={{marginRight: 8, fontSize: 20}}>이름:</Text>
+        <TextInput
+          style={{fontSize: 20}}
+          placeholder="이름을 입력해주세요"
+          onChangeText={newName => onChangeName(newName)}
+          defaultValue={name}
+        />
+      </View>
+      <Button title="추가하기" />
     </View>
   );
 }
